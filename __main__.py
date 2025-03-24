cd452ef3718c5df529f8879945da63868734e734
OxeanSA <oxeansa@gmail.com>
2025-03-24 14:12:48 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -9,14 +9,4 @@ server_ip = get_ip()
 app = create_app()
 
 if __name__ == '__main__':
-	os.system('clear')
-	print("V2 API Server")
-
-	app.listen(port=int(port), address=server_ip)
-	time.sleep(1)
-	print("IP: " + server_ip)
-	time.sleep(1)
-	print("Port: " + port)
-	time.sleep(1)
-	print("Online •")
-	IOLoop.instance().start()
+	app.run(debug=True)
